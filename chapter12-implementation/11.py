5de9417667e5c303872d753ae5e4c9f74ebace6b
jayoung977 <wjsrnr20@gmail.com>
2023-04-26 09:26:47 +0900

    study: implementation

--- chapter12-implementation/11.py
+++ chapter12-implementation/11.py
@@ -42,28 +42,28 @@ def turn(alpha):
 
 
 while True:
-    cnt += 1
+    cnt += 1  #초 
     x += dx[direction]
     y += dy[direction]
 
     if x < 0 or x >= N or y < 0 or y >= N: #벽에 부딫히면 게임끝
         break
 
-    if board[x][y] == 2:
+    if board[x][y] == 2:  #사과가 있으면
         board[x][y] = 1
         queue.append((x, y))
-        if cnt in directDict:
-            turn(directDict[cnt])
+        if cnt in directDict:  #X초인 경우 
+            turn(directDict[cnt])  #딕셔너리에서 C값 확인 및 turn 함수 실행
 
-    elif board[x][y] == 0:
+    elif board[x][y] == 0: #사과가 없으면
         board[x][y] = 1
         queue.append((x, y))
-        tx, ty = queue.popleft()
-        board[tx][ty] = 0
-        if cnt in directDict:
-            turn(directDict[cnt])
+        tx, ty = queue.popleft() #꼬리 좌표 값을 뺴면서 tx, ty 로 출력
+        board[tx][ty] = 0  #꼬리 좌표값 0으로 만듦 (꼬리 뻄)
+        if cnt in directDict:   #X초인 경우 
+            turn(directDict[cnt])   #딕셔너리에서 C값 확인 및 turn 함수 실행
 
-    else:
+    else:  #만약 자기자신에 부딪힐 경우도 게임 끝 board[x][y] == 1: 
         break
 
 print(cnt)
